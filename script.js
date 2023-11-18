1766eebe84fb074ce1218812f72ec2b93a8f3d95
afefwanes <afef.waness1993@gmail.com>
2023-11-18 16:14:27 +0100

    asdejf

---
+++ script.js
@@ -0,0 +1,11 @@
+var plus btns =  document.querySelectorAll(".plus")
+console console.log(plus_btns);
+function increment (e) {
+    console console.log(cible);
+    var firstdiv=cible.parentelemnt
+
+}
+for (let i = 0; i< plus_btns.length; i++) {
+    plus_btns [i]; addEventListener("click";increment)
+    
+}
